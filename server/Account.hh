4303aa76679aa447b26f96f5ce629407b8b2e3f0
Serge Heitzler <serge.heitzler@epitech.eu>
2015-10-14 02:27:44 -0700

    push class skeleton

--- server/Account.hh
+++ server/Account.hh
@@ -2,13 +2,60 @@
 # define _ACCOUNT_HH
 
 #include <string>
+#include <pair>
 
-
-class		Account
+class		        Account : PTObject
 {
-  std::string	_login;
-  
+  typedef enum		state
+    {
+      CONNECTED,
+      AWAY,
+      ONCALL,
+      DISCONNECTED
+    }			state;
 
+  Account();
+  ~Account();
+
+  void			setLogin(std::string login);
+  void			setState(Account::state state);
+  void			setLogin(std::string login);
+
+  std::string					&getLogin();
+  std::string					&getLocation();
+  std::string					&getPasswd();
+  std::vector<Account*>				&getContactList();
+  Account::state				getState();
+  int						getFd();
+
+private:
+
+  std::string					_login;
+  std::string					_passwd;
+  Account::state       				_state;
+  std::string					_location;
+
+  std::vector<std::pair<Account*:std::string>>	_nicknames;
+
+  std::vector<Account*>				_contactsList;
+  std::vector<Account*>				_favoritesList;
+  std::vector<Call*>				_callsHistory;
+  std::vector<Record*>				_records;
+  std::vector<Mail*>				_mails;
+
+
+  int						_fd;
+
+
+  /*
+
+     Historic Call
+     Historic Chat
+     
+     AnsweringMachine
+
+
+  */
 
 };
 
---
+++ server/PTObject.cpp
@@ -0,0 +1,11 @@
+#include "PTObject.hh"
+
+PTObject::PTObject()
+{
+  // Generate ObjectID string 
+}
+
+PTObject::~PTObject()
+{
+
+}
