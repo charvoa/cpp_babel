e33ee58e6eddbd0b21e119934e3ce7299e4627f2
Nicolas charvoz <nicolas.charvoz@epitech.eu>
2015-10-19 18:49:02 +0200

    MVC

--- app/User/PTUser.hh
+++ app/User/PTUser.hh
@@ -28,6 +28,7 @@ public:
   PTUser();
   ~PTUser();
   User&		currentUser();
+  int run(int, char**);
   template<typename T>
   void logUser(T &obj, void(T::*call)(int), const std::string &username, const std::string &password, const std::string &ip)
   {
