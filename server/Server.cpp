51b67167cadcfcf9b3ca717db5b2eaff2d3da39a
Serge Heitzler <serge.heitzler@epitech.eu>
2015-10-26 21:02:12 +0100

    add some cmds and skeleton 90%

---
+++ server/Server.cpp
@@ -0,0 +1,71 @@
+#include "Server.hh"
+
+Server::Server()
+{
+
+}
+
+Server::~Server()
+{
+
+}
+
+bool      Server::isUsernameExisting(std::string &username)
+{
+  for (std::vector::iterator it = _allAccounts.begin(); it != _allAccounts.end(); ++it)
+    {
+      if (it->getLogin() == username)
+        return true;
+    }
+  return false;
+}
+
+bool      Server::isPasswdCorrectForAccount(std::string &username, std::string &passwd)
+{
+  for (std::vector::iterator it = _allAccounts.begin(); it != _allAccounts.end(); ++it)
+    {
+      if (it->getLogin() == username && it->getPasswd() == passwd)
+        return true;
+    }
+  return false;
+}
+
+bool      Server::setNicknameForUser(std::string &nickname, std::string clientID)
+{
+
+}
+
+bool      Server::addAccount(std::string &login, std::string &passwd, short profilePicture)
+{
+  _allAccounts.push_back(new Account(login, passwd, profilePicture));
+}
+
+Account   &Server::getAccountByID(std::string &ID)
+{
+  for (std::vector::iterator it = _allAccounts.begin(); it != _allAccounts.end(); ++it)
+    {
+      if (it->getID() == ID)
+        return _allAccounts.at(std::distance(_allAccounts.begin(), it));
+    }
+  return NULL;
+}
+
+Account   &Server::getAccountIteratorByID(std::string &ID)
+{
+  for (std::vector::iterator it = _allAccounts.begin(); it != _allAccounts.end(); ++it)
+    {
+      if (it->getID() == ID)
+        return it;
+    }
+  return NULL;
+}
+
+Account   &Server::getAccountByUsername(std::string &username)
+{
+  for (std::vector::iterator it = _allAccounts.begin(); it != _allAccounts.end(); ++it)
+    {
+      if (it->getUsername() == username)
+        return it;
+    }
+  return NULL;
+}
