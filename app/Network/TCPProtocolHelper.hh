b92c41cf0f153fb95a13bece97cde3c173864f67
Antoine Garcia <antoine.garcia@epitech.eu>
2015-10-27 02:57:24 -0700

    TcpProtolHelper beginning

--- app/Network/TCPProtocolHelper.hh
+++ app/Network/TCPProtocolHelper.hh
@@ -5,7 +5,7 @@
 // Login   <antoinegarcia@epitech.net>
 //
 // Started on  Mon Oct 19 13:34:10 2015 Antoine Garcia
-// Last update Tue Oct 20 07:10:20 2015 Antoine Garcia
+// Last update Tue Oct 27 02:55:51 2015 Antoine Garcia
 //
 
 #ifndef TCPProtocolHelper_HH_
@@ -13,28 +13,7 @@
 
 #include <string>
 #include <cstdlib>
-
-typedef enum TCPProtocolHeader
-  {
-    SIGN_UP,
-    SIGN_IN,
-    CALL,
-    MUTE,
-    HANG_UP,
-    SEND_TEXT,
-    SEND_FILE,
-    ADD_PARTICIPANT,
-    ADD_CONTACT,
-    GET_CONTACT,
-    REMOVE_CONTACT,
-    MODIFY_PROFIL_PICTURE,
-    MODIFY_STATUS,
-    MODIFY_LOGIN,
-    MODIFY_LOCATION,
-    ADD_NICKNAME,
-    LISTEN_TO_MAIL
-
-  }TCPProtocolHeader;
+#include "IProtocolHelper.hh"
 
 class	TCPProtolHelper
 {
@@ -42,7 +21,7 @@ class	TCPProtolHelper
 public:
   TCPProtolHelper(const std::string &data = NULL);
   ~TCPProtolHelper();
-  void	writeHead(TCPProtocolHeader type);
+  void	writeHead(ProtocolType type);
   char		     readHeader(const std::string &);
   const std::string	&getData();
 };
