699a4ba9d3ae8439d0e8845eda5c6bc4a9b63911
Antoine Garcia <antoine.garcia@epitech.eu>
2015-10-31 09:25:34 -0700

    test

--- app/Audio/pa_fuzz.c
+++ app/Audio/pa_fuzz.c
@@ -31,13 +31,13 @@
  */
 
 /*
- * The text above constitutes the entire PortAudio license; however, 
+ * The text above constitutes the entire PortAudio license; however,
  * the PortAudio community also makes the following non-binding requests:
  *
  * Any person wishing to distribute modifications to the Software is
  * requested to send the modifications to the original developer so that
- * they can be incorporated into the canonical version. It is also 
- * requested that these non-binding requests be included along with the 
+ * they can be incorporated into the canonical version. It is also
+ * requested that these non-binding requests be included along with the
  * license above.
  */
 
@@ -55,6 +55,12 @@
 
 typedef float SAMPLE;
 
+typedef struct
+{
+  float left_phase;
+  float right_phase;
+}
+  paTestData;
 float CubicAmplifier( float input );
 static int fuzzCallback( const void *inputBuffer, void *outputBuffer,
                          unsigned long framesPerBuffer,
@@ -92,31 +98,26 @@ static int fuzzCallback( const void *inputBuffer, void *outputBuffer,
                          PaStreamCallbackFlags statusFlags,
                          void *userData )
 {
-    SAMPLE *out = (SAMPLE*)outputBuffer;
-    const SAMPLE *in = (const SAMPLE*)inputBuffer;
-    unsigned int i;
-    (void) timeInfo; /* Prevent unused variable warnings. */
-    (void) statusFlags;
-    (void) userData;
+  /* Cast data passed through stream to our structure. */
+  paTestData *data = (paTestData*)userData;
+  float *out = (float*)outputBuffer;
+  unsigned int i;
+  (void) inputBuffer; /* Prevent unused variable warning. */
 
-    if( inputBuffer == NULL )
+  for( i=0; i<framesPerBuffer; i++ )
     {
-        for( i=0; i<framesPerBuffer; i++ )
-        {
-            *out++ = 0;  /* left - silent */
-            *out++ = 0;  /* right - silent */
-        }
-        gNumNoInputs += 1;
+      *out++ = data->left_phase;  /* left */
+      *out++ = data->right_phase;  /* right */
+      /* Generate simple sawtooth phaser that ranges between -1.0 and 1.0. */
+      data->left_phase += 0.01f;
+      /* When signal reaches top, drop back down. */
+      if( data->left_phase >= 1.0f ) data->left_phase -= 2.0f;
+      /* higher pitch so we can distinguish left and right. */
+      data->right_phase += 0.03f;
+      if( data->right_phase >= 1.0f ) data->right_phase -= 2.0f;
     }
-    else
-    {
-        for( i=0; i<framesPerBuffer; i++ )
-        {
-            *out++ = FUZZ(*in++);  /* left - distorted */
-            *out++ = *in++;          /* right - clean */
-        }
-    }
-    
+  return 0;
+
     return paContinue;
 }
 
