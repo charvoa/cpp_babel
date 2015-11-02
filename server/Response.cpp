4c36b2b9249f55015f4851b4a13808584b9b7ec7
Serge Heitzler <serge.heitzler@epitech.eu>
2015-11-02 11:22:34 +0100

    debug response and compilation

--- server/Response.cpp
+++ server/Response.cpp
@@ -5,7 +5,7 @@
 // Login   <heitzl_s@epitech.eu>
 //
 // Started on  Sat Oct 31 16:16:36 2015 Serge Heitzler
-// Last update Mon Nov  2 11:16:43 2015 Serge Heitzler
+// Last update Mon Nov  2 11:21:43 2015 Serge Heitzler
 //
 
 #include <boost/lexical_cast.hpp>
@@ -30,7 +30,7 @@ void           Response::setSizeData(std::vector<std::string> data)
 {
   std::cout << "Cotsa" << std::endl;
   if (data.size() == 0)
-    return _sizeData = 0;
+    _sizeData = 0;
   for (std::vector<std::string>::iterator it = data.begin(); it != data.end(); ++it)
     {
       _sizeData += (*it).length();
@@ -43,11 +43,9 @@ void           Response::setSizeData(std::vector<std::string> data)
 void           Response::setResponse(CommunicationServer answerType, std::vector<std::string> data)
 {
   std::bitset<16> bit(_sizeData);
-  std::bitset<8> first8(request.at(5));
-  std::bitset<8> second8(request.at(6));
   _response += boost::lexical_cast<char>(answerType);
-  _response += boost::lexical_cast<char>(this.extractBitsetValue(0, 7, bit));
-  _response += boost::lexical_cast<char>(this.extractBitsetValue(8, 15, bit));
+  _response += boost::lexical_cast<char>(this->extractBitsetValue(0, 7, bit));
+  _response += boost::lexical_cast<char>(this->extractBitsetValue(8, 15, bit));
   if (_sizeData == 0)
     ;
   else
@@ -57,7 +55,7 @@ void           Response::setResponse(CommunicationServer answerType, std::vector
         _response += (*it);
         _response += CHAR_SEPARATOR;
       }
-      _response.pop_back();    
+      _response.pop_back();
   }
 }
 
@@ -77,14 +75,14 @@ std::string           &Response::getResponse()
   return _response;
 }
 
-int Response::extractBitsetValue(int startBit, int endBit, bitset<16> dataContainer)
+int Response::extractBitsetValue(int startBit, int endBit, std::bitset<16> dataContainer)
 {
-	int maskLength = end_bit - start_bit+1;
-	int moveBitNr = start_bit;
-	bitset<16> mask;
-	for(int k=0; k<maskLength; k++)
+	int maskLength = endBit - startBit + 1;
+	int moveBitNr = startBit;
+	std::bitset<16> mask;
+	for(int k = 0; k < maskLength; k++)
 	{
-		mask.set(start_bit+k,1);
+		mask.set(startBit + k, 1);
 	}
 
 	dataContainer.operator &=(mask);
--- server/Response.hh
+++ server/Response.hh
@@ -5,7 +5,7 @@
 // Login   <heitzl_s@epitech.eu>
 //
 // Started on  Sat Oct 31 16:40:28 2015 Serge Heitzler
-// Last update Sun Nov  1 17:24:19 2015 Nicolas Girardot
+// Last update Mon Nov  2 11:21:23 2015 Serge Heitzler
 //
 
 #ifndef _RESPONSE_HH
@@ -33,6 +33,7 @@ public:
   Account               *getClient();
   TCPConnection::pointer &getSocket();
   std::string           &getResponse();
+  int extractBitsetValue(int startBit, int endBit, std::bitset<16> dataContainer);
 
 private:
 
