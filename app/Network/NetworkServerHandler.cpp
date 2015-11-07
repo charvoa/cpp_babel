0cc074192dd44755f0a0fa3ba2feb4c015d4cc4f
Antoine Garcia <antoine.garcia@epitech.eu>
2015-11-07 07:27:02 -0800

    Update

--- app/Network/NetworkServerHandler.cpp
+++ app/Network/NetworkServerHandler.cpp
@@ -5,7 +5,7 @@
 // Login   <antoinegarcia@epitech.net>
 //
 // Started on  Sun Oct 18 00:42:17 2015 Antoine Garcia
-// Last update Sat Nov  7 07:20:52 2015 Antoine Garcia
+// Last update Sat Nov  7 07:26:55 2015 Antoine Garcia
 //
 
 #include "NetworkServerHandler.hh"
@@ -139,7 +139,6 @@ void	NetworkServerHandler::loginSuccess()
 {
   _request.parseLoginSuccess(_read);
   g_PTUser.setUsername(login);
-  std::cout << "MY LOGIN ="<< g_PTUser.currentUser().getUsername() << std::endl;
   emit userConnected(1);
 }
 
