5de01f79a2a80b02fe85d69c5a813c5709524b6c
Antoine Garcia <antoine.garcia@epitech.eu>
2015-11-07 07:57:28 -0800

    Fix

--- app/Network/TCPProtocolHelper.cpp
+++ app/Network/TCPProtocolHelper.cpp
@@ -5,7 +5,7 @@
 // Login   <antoinegarcia@epitech.net>
 //
 // Started on  Tue Oct 27 02:58:48 2015 Antoine Garcia
-// Last update Sat Nov  7 07:43:51 2015 Antoine Garcia
+// Last update Sat Nov  7 07:57:22 2015 Antoine Garcia
 //
 
 #include "TCPProtocolHelper.hh"
@@ -103,17 +103,6 @@ QByteArray	TCPProtocolHelper::acceptCallRequest()
   return block;
 }
 
-QByteArray	TCPProtocolHelper::addContactRequest()
-{
-   QByteArray	array;
-  QDataStream	out(&array, QIODevice::WriteOnly);
-  std::string str;
-
-  str += c;
-  out.setVersion(QDataStream::Qt_4_3);
-  out <<  quint8(ADD_CONTACT) << quint32(0) << quint16(str.size());
-  out.writeRawData(str.c_str(), str.size());
-}
 
 //handleRequest	Methods
 void	TCPProtocolHelper::handleHandshake()
